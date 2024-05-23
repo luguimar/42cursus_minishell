5f9e692b909b2e7bfbbb5601cff2f0b4f5509db9
jduraes- <jdbarros99@gmail.com>
2024-05-23 21:37:14 +0100

    maybe 0 leaks

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 19:58:13 by jduraes-          #+#    #+#             */
-/*   Updated: 2024/05/22 20:37:06 by jduraes-         ###   ########.fr       */
+/*   Updated: 2024/05/23 21:36:40 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,37 @@
 
 static char	*expand_aux(char *key, t_shell *shell)
 {
+	char	*value;
+
 	if (get_env_value(shell->env, key) != NULL)
-		return (ft_strdup(get_env_value(shell->env, key)));
+	{
+		value = ft_strdup(get_env_value(shell->env, key));
+		free(key);
+		return (value);
+	}
 	else
 		return (NULL);
 }
 
-static char	*expandqm(char **input, char *key, int i, int s)
+static char	*expandqm(char **input, char *key, int *i, int *s)
 {
 	char	*new;
 
-	new = ft_strjoinfreeall(ft_substr(*input, 0, i), key);
-	new = ft_strjoinfreeall(new, \
-		ft_substr(*input, i + s + 1, ft_strlen(*input) - (i + s)));
-	free(*input);
-	*input = ft_strdup(new);
-	return (new);
+	if (key)
+	{
+		new = ft_strjoinfreeall(ft_substr(*input, 0, *i), key);
+		new = ft_strjoinfreeall(new, \
+			ft_substr(*input, *i + *s + 1, ft_strlen(*input) - (*i + *s)));
+		free(*input);
+		*s = 0;
+		*input = ft_strdup(new);
+		return (new);
+	}
+	else
+	{
+		*s = 0;
+		return (ft_strdup(*input));
+	}
 }
 
 void	expand(char **inputt, t_shell *shell, int i, int s)
@@ -46,13 +61,19 @@ void	expand(char **inputt, t_shell *shell, int i, int s)
 				s++;
 			if (s)
 			{
+				free(*inputt);
 				*inputt = expandqm(&input, \
-					expand_aux(ft_substr(input, i + 1, s), shell), i, s);
+					expand_aux(ft_substr(input, i + 1, s), shell), &i, &s);
 			}
 			else if (input[i + 1] == '?')
-				*inputt = expandqm(&input, ft_itoa(shell->exit_status), i, s + 1);
-			i = 0;
-			s = 0;
+			{
+				s++;
+				free(*inputt);
+				*inputt = expandqm(&input, \
+					ft_itoa(shell->exit_status), &i, &s);
+			}
 		}
 	}
+	if (input)
+		free(input);
 }
