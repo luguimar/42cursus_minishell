7b4d3ff451a62c26b547ab8d74074cb686758449
jduraes- <jdbarros99@gmail.com>
2024-06-07 18:54:38 +0100

    expand expanded

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: jduraes- <jduraes-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/21 19:58:13 by jduraes-          #+#    #+#             */
-/*   Updated: 2024/06/07 18:23:27 by jduraes-         ###   ########.fr       */
+/*   Updated: 2024/06/07 18:53:24 by jduraes-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,13 @@ static void	expandqm(char **input, char *key, int i, int *s)
 		*input = new;
 	}
 	else
+	{
+		new = ft_strjoinfreeall(ft_substr(*input, 0, i), \
+			ft_substr(*input, i + *s + 1, ft_strlen(*input) - (i + *s)));
+		free(*input);
+		*input = new;
 		*s = 0;
+	}
 }
 
 /*void	expand(t_shell *shell, int i, int s)
@@ -79,8 +85,9 @@ void	expand(char **input, t_shell *shell, int i, int s)
 		if ((*input)[i] == '$' && inquote(*input, i) != '\'')
 		{
 			while ((*input)[i + 1 + s] != '\0'
-				&& !ft_is_special_char((*input)[i + 1 + s]))
-				s++;
+				&& !ft_is_special_char((*input)[i + 1 + ++s]))
+				if (ft_isdigit((*input)[i + 1]))
+					break;
 			if (s)
 			{
 				expandqm(input, \
